9dfea8e00f40471703a266f941ca766a911c3abe
Doan Nhat Trung Tran <trungtdnfx15649@funix.edu.vn>
2023-08-28 23:58:50 +1000

    nav bar

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -8,6 +8,7 @@ import {logo,menu,close } from '../assets'
 
 const Navbar = () => {
   const [active, setActive] = useState("")
+  const [toggle, setToggle] = useState(false)
   return (
     <nav className={`${styles.paddingX} w-full flex items-center py-5 fixed top-0 z-20 bg-primary`}>
 
@@ -20,9 +21,32 @@ const Navbar = () => {
             window.scrollTo(0,0);
         }}>
           <img src={logo} alt="logo" className="w-9 h-9 object-contain"/>
-          <p className="text-white text-[18px] font-bold cursor-pointer">Thomas | <span className="sm: block hidden">| Web developer</span></p>
+          <p className="text-white text-[18px] font-bold cursor-pointer">Thomas <span className="sm:block hidden">| Web developer</span></p>
         </Link>
-        <p className="text-red-500">asdsa</p>
+          <ul className="list-none hidden sm:flex flew-rơ gap-10">
+          {navLinks.map((link)=> (
+            <li 
+            key={link.id}
+            className={`${
+              active=== link.title 
+              ?  "text-white"
+              : "text-secondary"
+            } hover:text-white text-[18px] font-medium cursor-pointer`}
+            onClick={()=> setActive(link.title)}>
+              <a href={`#${link.id}`}>{link.title}</a>
+            </li>
+          ))}
+        </ul>
+
+        {/* Mobile navigation bar */}
+        <div className="sm:hidden flex flex-1 justify-end items-center">
+          <img
+          src={toggle ? close : menu}
+          alt='menu'
+          className="w-[28px] h-[28px] object-contain cursor-pointer"
+          onClick={() => setToggle(!toggle)}
+          />
+        </div>
       </div>
     </nav>
   )
